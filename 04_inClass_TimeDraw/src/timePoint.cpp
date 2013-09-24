26c95808dd6b2bdf90700a9d8f79b3a742d177d9
Francesca Castelli <franci.castelli@gmail.com>
2013-09-23 21:11:38 -0400

    first commit fireworks

---
+++ 04_inClass_TimeDraw/src/timePoint.cpp
@@ -0,0 +1,9 @@
+//
+//  timePoint.cpp
+//  04_inClass_TimeDraw
+//
+//  Created by Francesca Castelli on 9/17/13.
+//
+//
+
+#include "timePoint.h"
---
+++ 04_inClass_TimeDraw/src/timePoint.h
@@ -0,0 +1,14 @@
+//
+//  timePoint.h
+//  04_inClass_TimeDraw
+//
+//  Created by Francesca Castelli on 9/17/13.
+//
+//
+
+#ifndef ___4_inClass_TimeDraw__timePoint__
+#define ___4_inClass_TimeDraw__timePoint__
+
+#include <iostream>
+
+#endif /* defined(___4_inClass_TimeDraw__timePoint__) */
