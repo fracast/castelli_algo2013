1ed96021b3323138d319f32da3c63b71451d15f1
Francesca Castelli <franci.castelli@gmail.com>
2013-11-19 17:19:48 -0500

    osc screen 1

---
+++ 13_inClassKoch/src/KochLine.cpp
@@ -0,0 +1,9 @@
+//
+//  KochLine.cpp
+//  13_inClassKoch
+//
+//  Created by Francesca Castelli on 11/19/13.
+//
+//
+
+#include "KochLine.h"
---
+++ 13_inClassKoch/src/KochLine.h
@@ -0,0 +1,14 @@
+//
+//  KochLine.h
+//  13_inClassKoch
+//
+//  Created by Francesca Castelli on 11/19/13.
+//
+//
+
+#ifndef ___3_inClassKoch__KochLine__
+#define ___3_inClassKoch__KochLine__
+
+#include <iostream>
+
+#endif /* defined(___3_inClassKoch__KochLine__) */
